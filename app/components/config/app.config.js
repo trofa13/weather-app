5a0c42cad41eb6cc088a4a8e22e36796331a75b3
trofa13 <trofa13@inbox.ru>
2016-10-11 20:47:34 +0300

    some fixes for preview page

--- app/components/config/app.config.js
+++ app/components/config/app.config.js
@@ -8,7 +8,7 @@ angular.module('weatherApp.config',[])
 })
 
 .constant('weatherConfig',{
-    apiBaseUrl: 'http://api.openweathermap.org/data/2.5',
+    apiBaseUrl: 'https://api.openweathermap.org/data/2.5',
     apiKey: '088c18cc1d6f1f2e5ed1c97e9da77df2',
     lang: 'ru'
 });
